57c99c634bee1630b962d14b036e1a1cdc9fec96
Thibault Brillet <thbrillet@gmail.com>
2019-01-27 15:14:42 +0100

    matrix api ok, autocomplete ok

--- app/controllers/amaps_controller.rb
+++ app/controllers/amaps_controller.rb
@@ -1,9 +1,24 @@
+require 'json'
+require 'open-uri'
+
 class AmapsController < ApplicationController
   def index
     @amaps = Amap.all
+
+    amaps_coordinates = []
+
+    @amaps.each do |amap|
+      amaps_coordinates << "#{amap.latitude},#{amap.longitude}"
+    end
+
+    origin_address = '47.2165259,-1.556010399999991'
+
+    @url_matrix_api = "https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&mode=bicycling&origins=#{origin_address}&destinations=#{amaps_coordinates.join('|')}&key=#{ENV['MAPS_API_KEY']}"
+    @url_serialized = open(@url_matrix_api).read
+    @matrix = JSON.parse(@url_serialized)
   end
 
   def show
-      @amap = Amap.find(params[:id])
-    end
+    @amap = Amap.find(params[:id])
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,8 @@ Amap.create!(
   website: 'http://amapoudon.free.fr',
   email: 'thierry.bonsergent@gmail.com',
   nb_supplier: 5,
-  "latitude": "47.357162,-","longitude": "-1.3226387"
+  latitude: "47.357162",
+  longitude: "-1.3226387"
   )
 
 
@@ -23,7 +24,8 @@ Amap.create!(
   website: 'https://amapnort.wixsite.com/site',
   email: 'amap.nort@gmail.com',
   nb_supplier: 5,
-  "latitude": "47.4382105", "longitude": "-1.5063662"
+  latitude: "47.4382105",
+  longitude: "-1.5063662"
   )
 
 
@@ -34,7 +36,8 @@ Amap.create!(
   website: 'https://www.amap44.org/amap-du-savary/',
   email: '',
   nb_supplier: 1,
-  "latitude": "47.2749086", "longitude": "-2.2112197"
+  latitude: "47.2749086",
+  longitude: "-2.2112197"
   )
 
 Amap.create!(
@@ -43,7 +46,8 @@ Amap.create!(
   website: 'http://www.potagerbioderetz.com/',
   email: 'j.blomqvist@yahoo.fr',
   nb_supplier: 5,
-  "latitude": "47.266477", "longitude": "-2.033736"
+  latitude: "47.266477",
+  longitude: "-2.033736"
   )
 
 Amap.create!(
@@ -52,5 +56,6 @@ Amap.create!(
   website: 'http://www.amap-ancenis.fr/',
   email: 'amapancenis@gmail.com',
   nb_supplier: 9,
-  "latitude": "47.3891366", "longitude": "-1.1973153"
+  latitude: "47.3891366",
+  longitude: "-1.1973153"
   )
